96cec2d5552d0b8202e348ead813b82ae8d94cff
Diego Moises Calle Vasquez <168448644+DMCV0012@users.noreply.github.com>
2025-09-11 02:51:49 -0400

    Delete Web directory

--- Web/molecules/tour-card.js
+++
@@ -1,5 +0,0 @@
-// Molécula: tarjeta de tour con efecto hover
-export function setupTourCard(card) {
-  card.addEventListener('mouseenter', () => card.classList.add('hovered'));
-  card.addEventListener('mouseleave', () => card.classList.remove('hovered'));
-}
